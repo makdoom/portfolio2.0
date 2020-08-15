7c5cb4e36318a86613478f55d4de6d4f7ee598b6
Makdoom Shaikh <makshaikh99@gmail.com>
2020-08-15 12:04:23 +0530

    updating responsiveness

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+import "../sass/main.scss";
+import Typed from "typed.js/src/typed";
+// import Typed from "../node_modules/typed.js";
+// import Typed from "../node_modules/typed.js";
+
+// Toggling Humburger
+const menuBtn = document.querySelector(".burger");
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  document.querySelector(".nav__list").classList.toggle("open");
+});
+
+// Typing effect
+let typed = new Typed(".type", {
+  strings: ["Makdoom Shaikh", "Frontend Developer", "Programmer", "Developer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
