e6ad5128da26b6c7d95e8d3c970ed06013c163cf
Makdoom Shaikh <makshaikh99@gmail.com>
2020-08-20 20:01:10 +0530

    Done with Project section

--- dist/app.c3f9f951.js
+++ dist/app.c3f9f951.js
@@ -265,7 +265,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57650" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51887" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
