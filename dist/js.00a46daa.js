8e3348a5291ccee100ac7a4aef3999106f453091
Makdoom Shaikh <makshaikh99@gmail.com>
2020-08-21 21:47:36 +0530

    modifying responsiveness of design

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -1159,7 +1159,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60399" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49852" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
