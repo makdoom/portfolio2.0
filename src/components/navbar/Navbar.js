89a6592175f93042ad47ae2c407fcb340e4c4ae9
Makdoom Shaikh <makshaikh99@gmail.com>
2021-02-08 20:59:06 +0530

    Added active class feature

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,25 +2,50 @@ import React from "react";
 import "./navbar.css";
 
 const Navbar = () => {
+  const addActive = (e) => {
+    const links = document.querySelectorAll(".link");
+    links.forEach((link) => {
+      if ([...link.classList.includes("active")]) {
+        link.classList.remove("active");
+      }
+    });
+    e.target.parentElement.parentElement.classList.add("active");
+  };
+
   return (
     <div className="navbar">
       <div className="navbar__logo">LOGO</div>
       <div className="navbar__links">
         <ul>
-          <li className="link">
-            <a href="#intro">Intro</a>
+          <li className="link active" onClick={addActive}>
+            <a href="#intro">
+              <span className="link__number">01</span>
+              <span className="link__name">Intro</span>
+            </a>
           </li>
-          <li className="link">
-            <a href="#about">About</a>
+          <li className="link" onClick={addActive}>
+            <a href="#about">
+              <span className="link__number">02</span>
+              <span className="link__name">About</span>
+            </a>
           </li>
-          <li className="link">
-            <a href="#skills">Skills</a>
+          <li className="link" onClick={addActive}>
+            <a href="#skills">
+              <span className="link__number">03</span>
+              <span className="link__name">Skills</span>
+            </a>
           </li>
-          <li className="link">
-            <a href="#projects">Projects</a>
+          <li className="link" onClick={addActive}>
+            <a href="#projects">
+              <span className="link__number">04</span>
+              <span className="link__name">Projects</span>
+            </a>
           </li>
-          <li className="link">
-            <a href="#contact">Contact</a>
+          <li className="link" onClick={addActive}>
+            <a href="#contact">
+              <span className="link__number">05</span>
+              <span className="link__name">What's next ?</span>
+            </a>
           </li>
         </ul>
       </div>
